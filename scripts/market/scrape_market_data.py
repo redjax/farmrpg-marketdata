6d0bfe399a9b97abe11f07308a8766b10d73bb67
redjax <5534031+redjax@users.noreply.github.com>
2025-12-06 15:13:02 -0500

    Refactor scrape_market_data script to use controller

--- scripts/market/scrape_market_data.py
+++ scripts/market/scrape_market_data.py
@@ -6,24 +6,37 @@ from marketdata.setup import setup_logging
 from marketdata.db import init_db, get_session
 from marketdata.models import SteakPriceModel, KebabPriceModel
 from marketdata.classes import MarketPricesRaw, MarketPrices
+from marketdata.controllers import FarmRPGMarketPricesController
 
 import marketdata.shared as shared
 
 log = logging.getLogger(__name__)
 
 
-def main(
-    save_market_prices_html: bool = False,
-    save_prices_to_db: bool = False,
-    log_level: str = "INFO",
-    enable_file_logging: bool = False,
-):
-    log_file = None
+def main():
+    price_controller: FarmRPGMarketPricesController = FarmRPGMarketPricesController(
+        use_cache=True, save_html=True, save_to_db=True
+    )
 
-    if enable_file_logging:
-        log_file = "logs/steak_market.log"
+    if not price_controller.check_online():
+        log.error("FarmRPG is offline or down for mainteance")
 
-    setup_logging(level=log_level, file=log_file)
+        return
+
+    log.info("FarmRPG is online, proceeding")
+
+    ## Run scraper
+    try:
+        market_prices: MarketPrices = price_controller.run()
+    except Exception as exc:
+        log.error(
+            f"({type(exc).__name__}) Failed to scrape FarmRPG market prices. Details: {exc}"
+        )
+        raise
+
+
+if __name__ == "__main__":
+    setup_logging(level="DEBUG")
     log.debug("Debug logging enabled")
 
     try:
@@ -32,23 +45,4 @@ def main(
         log.error(f"({type(exc).__name__}) Failed to initialize database: {exc}")
         raise
 
-    try:
-        market_prices: MarketPrices = run_scraper(
-            save_prices_html=save_market_prices_html,
-            save_prices_to_db=save_prices_to_db,
-        )
-    except Exception as exc:
-        raise
-
-
-if __name__ == "__main__":
-    try:
-        main(
-            save_market_prices_html=True,
-            save_prices_to_db=True,
-            log_level="DEBUG",
-            enable_file_logging=True,
-        )
-    except Exception as exc:
-        print(f"[ERROR] ({type(exc)}) Failed to scrape current market prices: {exc}")
-        sys.exit(1)
+    main()
