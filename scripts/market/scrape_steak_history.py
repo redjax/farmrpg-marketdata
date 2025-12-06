96d9564d488b584042db624369964818749c2c49
redjax <5534031+redjax@users.noreply.github.com>
2025-12-05 23:33:41 -0500

    Add MarketPricesRaw dataclass
    Move requests flow into request_market_prices

--- scripts/market/scrape_steak_history.py
+++ scripts/market/scrape_steak_history.py
@@ -5,6 +5,8 @@ Kebab price history: https://farmrpg.com/steakhistoryk.php
 """
 
 import logging
+import sys
+from dataclasses import dataclass, field
 from shared import constants
 
 from bs4 import BeautifulSoup
@@ -13,6 +15,34 @@ import httpx
 import shared
 
 log = logging.getLogger(__name__)
+
+
+@dataclass
+class MarketPricesRaw:
+    steak_html: str
+    kebab_html: str
+
+
+def request_market_prices() -> MarketPricesRaw:
+    steak_url: str = constants.STEAK_HISTORY_URL
+    kebab_url: str = constants.KEBAB_HISTORY_URL
+    log.debug(f"Steak URL: {steak_url}")
+    log.debug(f"Kebab URL: {kebab_url}")
+
+    steak_prices_req: httpx.Request = httpx.Request(method="GET", url=steak_url)
+    kebab_prices_req: httpx.Request = httpx.Request(method="GET", url=kebab_url)
+
+    log.info("Requesting steak prices HTML")
+    steak_prices_res: httpx.Response = shared.http_utils.send_request(steak_prices_req)
+    steak_prices_html = steak_prices_res.content.decode("utf-8")
+    # log.debug(f"Steak prices HTML raw:\n{steak_prices_html}")
+
+    log.info("Requesting kebab prices HTML")
+    kebab_prices_res: httpx.Response = shared.http_utils.send_request(kebab_prices_req)
+    kebab_prices_html = kebab_prices_res.content.decode("utf-8")
+    # log.debug(f"Kebab prices HTML raw:\n{kebab_prices_html}")
+
+    return MarketPricesRaw(steak_html=steak_prices_html, kebab_html=kebab_prices_html)
 
 
 def main(log_level: str = "INFO", enable_file_logging: bool = False):
@@ -24,22 +54,19 @@ def main(log_level: str = "INFO", enable_file_logging: bool = False):
     shared.setup_logging(level=log_level, file=log_file)
     log.debug("Debug logging enabled")
 
-    client: httpx.Client = shared.http_utils.get_client(use_cache=True)
-
-    steak_url: str = constants.STEAK_HISTORY_URL
-    kebab_url: str = constants.KEBAB_HISTORY_URL
-    log.debug(f"Steak URL: {steak_url}")
-    log.debug(f"Kebab URL: {kebab_url}")
-
-    steak_prices_req: httpx.Request = httpx.Request(method="GET", url=steak_url)
-    kebab_prices_req: httpx.Request = httpx.Request(method="GET", url=kebab_url)
-
-    log.info("Requesting steak prices HTML")
-    steak_prices_res: httpx.Response = shared.http_utils.send_request(steak_prices_req)
-
-    log.info("Requesting kebab prices HTML")
-    kebab_prices_res: httpx.Response = shared.http_utils.send_request(kebab_prices_req)
+    log.info("Requesting market prices for steak & kebabs")
+    try:
+        market_prices: MarketPricesRaw = request_market_prices()
+    except Exception as exc:
+        log.error(
+            f"({type(exc).__name__}) Failed requesting current steak prices: {exc}"
+        )
+        raise
 
 
 if __name__ == "__main__":
-    main(log_level="DEBUG", enable_file_logging=True)
+    try:
+        main(log_level="DEBUG", enable_file_logging=True)
+    except Exception as exc:
+        print(f"[ERROR] ({type(exc)}) Failed to scrape current market prices")
+        sys.exit(1)
