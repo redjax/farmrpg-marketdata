a055101cec23449fb1182960064f6f63acd7c113
redjax <5534031+redjax@users.noreply.github.com>
2025-12-09 23:12:08 -0500

    Fix saving current price to database

--- src/marketdata/models.py
+++ src/marketdata/models.py
@@ -3,7 +3,12 @@ from marketdata.db import Base
 from sqlalchemy import Integer, String, Date, DateTime, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 
-__all__ = ["SteakPriceModel", "KebabPriceModel"]
+__all__ = [
+    "SteakPriceModel",
+    "KebabPriceModel",
+    "SteakPriceCurrent",
+    "KabobPriceCurrent",
+]
 
 
 class SteakPriceModel(Base):
@@ -20,6 +25,24 @@ class SteakPriceModel(Base):
 class KebabPriceModel(Base):
     __tablename__ = "kebab_prices"
     __table_args__ = (UniqueConstraint("timestamp", name="uq_kebab_timestamp"),)
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    timestamp: Mapped[DateTime] = mapped_column(DateTime, nullable=False)
+    price: Mapped[int] = mapped_column(Integer, nullable=False)
+
+
+class SteakPriceCurrentModel(Base):
+    __tablename__ = "steak_prices_current"
+    __table_args__ = (UniqueConstraint("id", name="uq_steak_current"),)
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    timestamp: Mapped[DateTime] = mapped_column(DateTime, nullable=False)
+    price: Mapped[int] = mapped_column(Integer, nullable=False)
+
+
+class KebabPriceCurrentModel(Base):
+    __tablename__ = "kebab_prices_current"
+    __table_args__ = (UniqueConstraint("id", name="uq_kebab_current"),)
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     timestamp: Mapped[DateTime] = mapped_column(DateTime, nullable=False)
