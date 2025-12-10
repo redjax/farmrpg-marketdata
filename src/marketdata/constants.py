8c004691c0c16449ffee34c4473eff2f9a5d01d2
redjax <5534031+redjax@users.noreply.github.com>
2025-12-09 22:33:25 -0500

    Add script that scrapes current market prices

--- src/marketdata/constants.py
+++ src/marketdata/constants.py
@@ -1,6 +1,13 @@
-__all__ = ["BASE_URL", "HOME_URL", "STEAK_HISTORY_URL", "KEBAB_HISTORY_URL"]
+__all__ = [
+    "BASE_URL",
+    "HOME_URL",
+    "STEAK_HISTORY_URL",
+    "KEBAB_HISTORY_URL",
+    "CURRENT_MARKET_PRICES_URL",
+]
 
 BASE_URL: str = "https://farmrpg.com"
 HOME_URL: str = f"{BASE_URL}/index.php"
 STEAK_HISTORY_URL: str = f"{BASE_URL}/steakhistory.php"
 KEBAB_HISTORY_URL: str = f"{BASE_URL}/steakhistoryk.php"
+CURRENT_MARKET_PRICES_URL: str = f"{BASE_URL}/steakmarket.php"
